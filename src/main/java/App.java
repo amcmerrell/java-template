74679ac64eedab55f8510c582f723d832dae5371
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-09-28 11:25:06 -0700

    add files and edit template with files to incorporate database connection.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,14 +8,13 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-
     String layout = "templates/layout.vtl";
 
-       get("/", (request, response) -> {
-         Map<String, Object> model = new HashMap<String, Object>();
-         model.put("template", "templates/form.vtl");
-         return new ModelAndView(model, layout);
-       }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 }
